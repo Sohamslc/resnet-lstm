a1ba97153149db7f73dbe6295906152970e9dc28
sohchauh <sohchauh@cisco.com>
2025-04-19 19:17:08 +0530

    removed port

--- app.py
+++ app.py
@@ -56,5 +56,5 @@ def predict():
     return jsonify({"caption": caption})
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=3000)
+    app.run(debug=True, host="0.0.0.0")
 
